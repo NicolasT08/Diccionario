b62102638a83fec833647c2894d19b20c09027fc
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-12-13 12:36:56 -0500

    (Add) project Structure

---
+++ src/test/java/co/edu/uptc/model/TreeNodeTest.java
@@ -0,0 +1,10 @@
+package co.edu.uptc.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreeNodeTest {
+    @Test
+    void test(){
+        
+    }
+}
