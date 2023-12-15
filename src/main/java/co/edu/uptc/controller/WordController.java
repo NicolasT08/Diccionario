b81d5a0c45c034df1f9ea4f9fb7061d476a61d04
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-12-15 12:46:59 -0500

    (Add) Add word controller feature

--- src/main/java/co/edu/uptc/controller/WordController.java
+++ src/main/java/co/edu/uptc/controller/WordController.java
@@ -1,5 +1,7 @@
 package co.edu.uptc.controller;
 
+import co.edu.uptc.exceptions.ERROR_REASON;
+import co.edu.uptc.exceptions.InvalidWord;
 import co.edu.uptc.model.BinaryTree;
 import co.edu.uptc.model.Word;
 
@@ -27,11 +29,51 @@ public class WordController {
             if ( firstChar == especialCases[i] ) return especialValues[i];
         }
 
-        throw  new IllegalArgumentException("La palabra " + word + " no pertenece a la lengua castellana.");
+        throw new InvalidWord(word, ERROR_REASON.IMPOSSIBLE);
     }
 
     public String addWord(String word, String meaning, String translation) {
-        return "";
+        String returnMessage = "";
+        try {
+            validateWord( word );
+            validateMeaning( meaning );
+            validateTranslation( translation );
+
+            meaning = meaning.replaceAll("\\s+", " ");
+            translation = translation.replaceAll("\\s+", " ");
+
+            int root = this.findIndex(word);
+            Word newWord = new Word( word, meaning,translation);
+            if ( this.dictionary[root].findNode(newWord) != null) throw new InvalidWord( word, ERROR_REASON.EXIST);
+            this.dictionary[root].addNode( newWord );
+            returnMessage = "La palabra " + word + " ha sido añadida satisfactoriamente";
+
+        }catch (IllegalArgumentException e){
+            returnMessage = e.getMessage();
+        }
+        return returnMessage;
+    }
+
+    private void validateWord( String word ){
+        if ( word == null ) throw  new InvalidWord( word, ERROR_REASON.IS_EMPTY);
+        for( int i = 0; i < word.length(); i++ ){
+            if ( !Character.isAlphabetic(word.charAt(i))) throw new InvalidWord( word, ERROR_REASON.INVALID_CHARACTERS);
+        }
+    }
+
+    private void validateMeaning( String content ){
+        if ( content == null ) throw  new InvalidWord( content, ERROR_REASON.IS_EMPTY);
+        if ( content.isBlank() ) throw new InvalidWord( content, ERROR_REASON.IS_EMPTY);
+        if ( content.startsWith(" ")) throw new InvalidWord( content, ERROR_REASON.STARTS_SPACE);
+    }
+
+    private void validateTranslation( String translation ){
+        if ( translation == null ) throw new InvalidWord(translation, ERROR_REASON.IS_EMPTY);
+        if ( translation.startsWith(" ")) throw new InvalidWord( translation, ERROR_REASON.STARTS_SPACE);
+        for( int i = 0; i < translation.length(); i++ ){
+            if ( translation.charAt(i) == ' ') continue;
+            if ( !Character.isAlphabetic(translation.charAt(i))) throw new InvalidWord( translation, ERROR_REASON.INVALID_CHARACTERS);
+        }
     }
 
     public String[] findWord(String word) {
@@ -47,6 +89,9 @@ public class WordController {
     }
 
     public String updateWord(String newValue, ATT_TYPE att){
+        return "";
+    }
+    public String deleteWord( String word ){
         return "";
     }
 }
--- src/test/java/co/edu/uptc/controller/WordControllerTest.java
+++ src/test/java/co/edu/uptc/controller/WordControllerTest.java
@@ -1,5 +1,7 @@
 package co.edu.uptc.controller;
 
+import co.edu.uptc.exceptions.InvalidWord;
+import co.edu.uptc.model.Word;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,26 +15,30 @@ public class WordControllerTest {
     }
     @Test
     void findWordIndex(){
-        String[] words = {"Amor", "Brillante", "Cielo", "Delfín", "Estrella", "Felicidad", "Gracia", "Harmonía", "Ilusión", "Jardín", "Kilo", "Luna", "Mariposa", "Naturaleza","Ñoño", "Océano", "Paz", "Quimera", "Resplandor", "Serenidad", "Tesoro", "Universo", "Viaje", "Waffles", "Xilófono", "Yunque", "Zafiro"};
-        for( int i = 0; i < words.length; i++ ){
-            assertEquals(i, this.controller.findIndex(words[i]));
-        }
+        try{
+            String[] words = {"Amor", "Brillante", "Cielo", "Delfín", "Estrella", "Felicidad", "Gracia", "Harmonía", "Ilusión", "Jardín", "Kilo", "Luna", "Mariposa", "Naturaleza","Ñoño", "Océano", "Paz", "Quimera", "Resplandor", "Serenidad", "Tesoro", "Universo", "Viaje", "Waffles", "Xilófono", "Yunque", "Zafiro"};
+            for( int i = 0; i < words.length; i++ ){
+                assertEquals(i, this.controller.findIndex(words[i]));
+            }
 
-        String[] lowerWords = {"a", "brillante", "cielo", "delfín", "ensueño", "feli", "gomelo", "Hu Tao", "imaginario", "jardín", "karma", "luna", "mariposa", "niñez","ñandú", "océano", "paz", "quietud", "resplandor", "serenidad", "tesoro", "universo", "viaje", "waffles", "xenón", "yoga", "zafiro"};
-        for ( int i = 0; i < lowerWords.length; i++ ){
-            assertEquals(i, this.controller.findIndex(lowerWords[i]));
-        }
+            String[] lowerWords = {"a", "brillante", "cielo", "delfín", "ensueño", "feli", "gomelo", "Hu Tao", "imaginario", "jardín", "karma", "luna", "mariposa", "niñez","ñandú", "océano", "paz", "quietud", "resplandor", "serenidad", "tesoro", "universo", "viaje", "waffles", "xenón", "yoga", "zafiro"};
+            for ( int i = 0; i < lowerWords.length; i++ ){
+                assertEquals(i, this.controller.findIndex(lowerWords[i]));
+            }
 
-        String[] accent = {"época", "íntimo", "óleo", "último", "árbol", "éxito", "óvalo", "última", "índigo", "ámbar"};
-        int[] values = {4,8,15,21,0,4,15,21,8,0};
-        for( int i = 0; i < accent.length; i++ ){
-            assertEquals(values[i], this.controller.findIndex(accent[i]));
-        }
+            String[] accent = {"época", "íntimo", "óleo", "último", "árbol", "éxito", "óvalo", "última", "índigo", "ámbar"};
+            int[] values = {4,8,15,21,0,4,15,21,8,0};
+            for( int i = 0; i < accent.length; i++ ){
+                assertEquals(values[i], this.controller.findIndex(accent[i]));
+            }
 
-        accent = new String[]{"Época", "Íntimo", "Óleo", "Último", "Árbol", "Éxito", "Óvalo", "Última", "Índigo", "Ámbar"};
-        values = new int[]{4,8,15,21,0,4,15,21,8,0};
-        for( int i = 0; i < accent.length; i++ ){
-            assertEquals(values[i], this.controller.findIndex(accent[i]));
+            accent = new String[]{"Época", "Íntimo", "Óleo", "Último", "Árbol", "Éxito", "Óvalo", "Última", "Índigo", "Ámbar"};
+            values = new int[]{4,8,15,21,0,4,15,21,8,0};
+            for( int i = 0; i < accent.length; i++ ){
+                assertEquals(values[i], this.controller.findIndex(accent[i]));
+            }
+        }catch ( Exception e){
+
         }
 
 
@@ -61,4 +67,45 @@ public class WordControllerTest {
         assertDoesNotThrow(() -> this.controller.findIndex("Hola"));
         assertDoesNotThrow(() -> this.controller.findIndex("euler"));
     }
+
+    @Test
+    void addWordTest(){
+        Word[] words ={
+                new Word("uwu", "emoticono tierno", "uwu"),
+                new Word("Árbol", "planta", "tree"),
+                new Word("agua", "líquido", "wa ter"),
+                new Word("Madera", "material de árboles", "wood"),
+                new Word("Salir", "Ir a fuera", "Go Out")
+
+        };
+        for ( Word w : words ){
+            assertEquals( "La palabra " + w.getId() + " ha sido añadida satisfactoriamente", this.controller.addWord(w.getId(), w.getMeaning(), w.getTranslation()));
+        }
+
+        for ( Word w : words ){
+            assertEquals( "La palabra \"" + w.getId() + "\" " + " ya existe!", this.controller.addWord(w.getId(), w.getMeaning(), w.getTranslation()));
+        }
+
+        Word[] invalidWords = {
+                new Word("1", "uno", "one"),
+                new Word("$tos", "palabra grosera", "rude"),
+                new Word("µvaca", "vaquita", "cow"),
+                new Word("µvaca", "vaquita", "cow"),
+                new Word("pera", "     ", " "),
+                new Word("uva", "fruta morada.", " "),
+                new Word(null, "fruta morada.", "grape"),
+                new Word(" paz", "fruta morada.", "grape"),
+                new Word("paz", "  fruta morada.", "grape"),
+                new Word("paz", "Tranquilidad", "  peace"),
+                new Word("manzana",  null, "apple"),
+                new Word(null,  null, null),
+                new Word("martinez",  "", ""),
+                new Word("estadística",  "", "stadistics"),
+        };
+
+        for ( Word w : invalidWords ){
+            assertNotEquals("La palabra " + w.getId() + " ha sido añadida satisfactoriamente", this.controller.addWord(w.getId(), w.getMeaning(), w.getTranslation()));
+        }
+    }
 }
+
