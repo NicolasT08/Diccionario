e0615e6361373831707bce9cc0f9022a871c99e5
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-12-16 11:34:09 -0500

    (Add) Main View

--- src/main/java/co/edu/uptc/Runner.java
+++ src/main/java/co/edu/uptc/Runner.java
@@ -1,10 +1,11 @@
 package co.edu.uptc;
 
-import co.edu.uptc.controller.WordController;
+import co.edu.uptc.view.MainView;
 
 public class Runner {
     public static void main(String[] args) {
-        WordController c = new WordController();
 
+        MainView view = new MainView();
+        view.initialize();
     }
 }
--- src/main/java/co/edu/uptc/view/MainView.java
+++ src/main/java/co/edu/uptc/view/MainView.java
@@ -1,4 +1,39 @@
 package co.edu.uptc.view;
 
-public class MainView {
+import co.edu.uptc.controller.WordController;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainView extends JFrame {
+
+    private JTabbedPane tabs;
+    private WordController controller;
+
+    public MainView(){
+        super("Minecraft Dictionary");
+        this.controller = new WordController();
+    }
+
+    public void initialize(){
+        this.setSize( 850, 600);
+        this.setLayout(new BorderLayout(10,100));
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        this.setTabs();
+
+        this.add(this.tabs, BorderLayout.CENTER);
+    }
+
+    public void setTabs(){
+        this.tabs = new JTabbedPane();
+        this.tabs.setTabPlacement(SwingConstants.LEFT);
+        this.tabs.setBorder( BorderFactory.createEmptyBorder(25,5,25,50));
+        JPanel jp = new JPanel();
+        jp.setBackground( new Color(120,120,30));
+
+        this.tabs.addTab("Actualizar", null, jp);
+    }
 }
