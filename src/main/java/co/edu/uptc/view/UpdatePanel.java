9fff6f2c453c7472fd76a0b1bd975c46e14c4cd9
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2023-12-16 19:16:14 -0500

    (Add) delete and find panels

---
+++ src/main/java/co/edu/uptc/view/UpdatePanel.java
@@ -0,0 +1,16 @@
+package co.edu.uptc.view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UpdatePanel extends JPanel implements ActionListener {
+
+    UpdatePanel(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
